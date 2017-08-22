bf38b7b43cff5cb3c6895488a45f6936a9e00fd5
Kyuho Kim <valve.god.project@gmail.com>
2017-08-22 07:58:32 +0000

    Update flask_mvp_webserver.py

--- flask_mvp_webserver.py
+++ flask_mvp_webserver.py
@@ -1,11 +1,17 @@
 from flask import Flask, request
+import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    for p in request.args:
-        print(p, request.args[p])
+    with open("log.txt", "a") as f:
+        dstr = datetime.datetime.now()
+        f.write("%s, " % (dstr))
+        for p in request.args:
+            f.write("%s, %s,  " % (p, request.args[p]))
+        f.write("\n");
     return 'I got: a=' + request.args['a']
 
 @app.route('/cakes')
@@ -14,3 +20,4 @@ def cakes():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+~                                          
