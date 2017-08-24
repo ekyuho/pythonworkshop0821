3ec78178e3e2933efa547780c4b0f491f23cb00f
Kyuho Kim <valve.god.project@gmail.com>
2017-08-24 11:00:39 +0900

    remove bug

--- thingspeak.py
+++ thingspeak.py
@@ -4,8 +4,8 @@ import random
 myurl = "https://api.thingspeak.com/update?api_key=A54Y2MHH6JNRH7JP&field1="
 
 for i in range(20):
-    myurl += str(random.randint(20, 80))
-    f = urllib.request.urlopen(myurl)
+   url = myurl + str(random.randint(20, 80))
+    f = urllib.request.urlopen(url)
     data = str(f.read())
     print("Got", data)
     time.sleep(15)
