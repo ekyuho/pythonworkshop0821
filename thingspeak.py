2f8ba070e776fe2cb58f3e95c4267ec885328b40
Kyuho Kim <valve.god.project@gmail.com>
2017-08-24 10:48:34 +0900

    Create thingspeak.py
    MVP Python program for ThingSpeak

---
+++ thingspeak.py
@@ -0,0 +1,11 @@
+import datetime, time
+import urllib.request
+import random
+myurl = "https://api.thingspeak.com/update?api_key=A54Y2MHH6JNRH7JP&field1="
+
+for i in range(20):
+    myurl += str(random.randint(20, 80))
+    f = urllib.request.urlopen(myurl)
+    data = str(f.read())
+    print("Got", data)
+    time.sleep(15)
