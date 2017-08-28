492c82a46617aee119a19dd177e07b9fd5735ad4
Kyuho Kim <valve.god.project@gmail.com>
2017-08-28 22:59:50 +0900

    Update flask_mythingspeakserver.py

--- flask_mythingspeakserver.py
+++ flask_mythingspeakserver.py
@@ -3,15 +3,14 @@ import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     with open("log.txt", "a") as f:
         dstr = datetime.datetime.now()
         f.write("%s, " % (dstr))
-        f.write("%s, %s,  " % (request.args['field1'], request.args['field2']))
+        f.write("%s, %s,  " % (request.args.get('field1', default=""), request.args.get('field2', default="")))
         f.write("\n");
-        print(dstr, request.args['field1'], request.args['field2'])
+        print(dstr, request.args.get('field1', default=""), request.args.get('field2', default=""))
     return 'Thank you'
 @app.route('/cakes')
 def cakes():
